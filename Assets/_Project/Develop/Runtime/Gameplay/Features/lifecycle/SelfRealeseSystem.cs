c516e1e8146c1adf018a09ab7b8c9bc2a0f125e8
zombiemurman <zombiemurman@yandex.ru>
2026-04-22 19:54:36 +0300

    Puddle

--- Assets/_Project/Develop/Runtime/Gameplay/Features/lifecycle/SelfRealeseSystem.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/lifecycle/SelfRealeseSystem.cs
@@ -29,8 +29,8 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Features.LifeCycle
         {
             if (_mustSelfReleased.Evaluate())
             {
-                //foreach (Entity entity in _entity.ChildEntities)
-                //    _lifeContext.Release(entity);
+                foreach (Entity entity in _entity.ChildEntities)
+                    _lifeContext.Release(entity);
 
                 _lifeContext.Release(_entity);
             }
